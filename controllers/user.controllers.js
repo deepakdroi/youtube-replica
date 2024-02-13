7f32391957f80b04cf3e67c9a005ea5a52df2c07
deepakdroi <deepak.mahto706@gmail.com>
2024-02-13 21:25:51 +0530

    Bug while not giving coverImage was fixed and console log statements were removed

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -32,7 +32,9 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 
   const avatarLocalPath = req.files?.avatar[0]?.path;
-  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+  const coverImageLocalPath = req.files.coverImage
+    ? req.files.coverImage[0]?.path
+    : null;
 
   if (!avatarLocalPath) {
     throw new ApiError(400, "Avatar file is required.");
